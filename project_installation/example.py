bbcf3385ec473f1d5c5e523a06f1a90bcc259f01
Andrea <andrea.ferrario@epfl.ch>
2026-03-19 12:53:42 +0100

    [project installation] reverted to groud arena first

--- project_installation/example.py
+++ project_installation/example.py
@@ -4,7 +4,7 @@ from cmc_project_pack.simulate import example_single
 if __name__ == "__main__":
     print("Running example single simulation...")
     example_single(
-        arena='water',
+        arena='ground',
         fast=False,
         headless=False
     )
